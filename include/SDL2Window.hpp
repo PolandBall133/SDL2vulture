8906f5c1f1992103619f607bd86b32a0eb2cd71a
spartanPAGE <spartanpageagain@gmail.com>
2014-07-13 17:25:44 +0200

    Added `Rectangle` header; Added public constructor for `SDL2Window`

--- include/SDL2Window.hpp
+++ include/SDL2Window.hpp
@@ -1,7 +1,10 @@
 #pragma once
 #include "Window.hpp"
+#include "Rectangle.hpp"
 
 namespace vulture{
     class SDL2Window : public Window{
+    public:
+        SDL2Window(const Rectangle &, Window::Flags);
     };
 }
