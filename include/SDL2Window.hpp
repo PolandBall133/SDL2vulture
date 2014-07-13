70c6ca7b61b2d976d996950d6c84b0e42181609d
spartanPAGE <spartanpageagain@gmail.com>
2014-07-13 17:46:27 +0200

    Added bunch of overrided accessors to `SDL2Window`

--- include/SDL2Window.hpp
+++ include/SDL2Window.hpp
@@ -6,5 +6,11 @@ namespace vulture{
     class SDL2Window : public Window{
     public:
         SDL2Window(const Rectangle &, Window::Flags);
+        virtual const std::string &title() const override;
+        virtual void title(const std::string &) override;
+        virtual size_t width() const override;
+        virtual void width(size_t) override;
+        virtual size_t height() const override;
+        virtual void height(size_t) override;
     };
 }
