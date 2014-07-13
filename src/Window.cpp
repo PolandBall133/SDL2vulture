d90d1433efc6d7232a43d72d107bf30232b850dd
spartanPAGE <spartanpageagain@gmail.com>
2014-07-13 22:06:24 +0200

    Added the whoole definitions. Thats all, be quiet, i know exactly how the commit should look like >:I

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,4 +1,50 @@
 #include "SDL2Window.hpp"
 
 namespace vulture{
+    SDL2Window::SDL2Window(const std::string &title, const Rectangle &info, Window::Flags flags):
+        _rect(info)
+    {
+        _handle = window_handle(
+            SDL_CreateWindow(title.c_str(), info.x, info.y, info.width, info.height, flags),
+            SDL_DestroyWindow
+        );
+    }
+
+    const std::string &
+    SDL2Window::title() const{
+        return _title;
+    }
+
+    void
+    SDL2Window::title(const std::string &value){
+        _title = value;
+        SDL_SetWindowTitle(_handle.get(), _title.c_str());
+    }
+
+    size_t
+    SDL2Window::width() const{
+        return _rect.width;
+    }
+
+    void
+    SDL2Window::width(size_t val){
+        _rect.width = val;
+        setWindowSize();
+    }
+
+    size_t
+    SDL2Window::height() const{
+        return _rect.height;
+    }
+
+    void
+    SDL2Window::height(size_t val){
+        _rect.height = val;
+        setWindowSize();
+    }
+
+    void
+    SDL2Window::setWindowSize(){
+        SDL_SetWindowSize(_handle.get(), width(), height());
+    }
 }
