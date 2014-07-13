8efe9a6169db46ed1bfbea03df6ad2436dcfd5eb
spartanPAGE <spartanpageagain@gmail.com>
2014-07-14 00:06:02 +0200

    Changed result type of `title` to `const object` in `SDL2Window`; Added `position` accessors to `SDL2Window`; Added `_position` field to `SDL2Window`; Added private `setWindowPosition()` method to `SDL2Window`

--- include/SDL2Window.hpp
+++ include/SDL2Window.hpp
@@ -10,19 +10,23 @@ namespace vulture{
     class SDL2Window : public Window{
     public:
         SDL2Window(const std::string &, const Rectangle &, Window::Flags);
-        virtual const std::string &title() const override;
+        virtual const std::string title() const override;
         virtual void title(const std::string &) override;
         virtual size_t width() const override;
         virtual void width(size_t) override;
         virtual size_t height() const override;
         virtual void height(size_t) override;
+        virtual const Point position() const override;
+        virtual void position(const Point &) override;
     protected:
         typedef std::shared_ptr<SDL_Window> window_handle;
 
         window_handle _handle;
         std::string _title;
         Rectangle _rect;
+        Point _position;
     private:
         void setWindowSize();
+        void setWindowPosition();
     };
 }
