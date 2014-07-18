26b9fa1bd5506c96b2940e4c5d852c120138d069
spartanPAGE <spartanpageagain@gmail.com>
2014-07-18 14:25:29 +0200

    Included "SDL" header; Added protected `renderer_handle` alias for `shared_ptr...` Added protected field `_handle`

--- include/graphics/SDL2Renderer.hpp
+++ include/graphics/SDL2Renderer.hpp
@@ -2,6 +2,8 @@
 
 #include "graphics/Renderer.hpp"
 
+#include "SDL2/SDL.h"
+
 #include <memory>
 
 namespace vulture{
@@ -10,5 +12,8 @@ namespace vulture{
         virtual void clear() override;
         virtual void present() override;
         virtual void drawingColor(const Color &) override;
+    protected:
+        typedef std::shared_ptr<SDL_Renderer> renderer_handle;
+        renderer_handle _handle;
     };
 }
