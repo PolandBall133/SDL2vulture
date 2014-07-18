7bbf160fad27d7e3accd687c1a74a0433d832c21
spartanPAGE <spartanpageagain@gmail.com>
2014-07-18 14:58:05 +0200

    `SDL2Renderer` constructor now fills `_handle` with `SDL_Renderer` by:   - "providing window handle"   - "creating new accelerated renderer from window handle"   - "setting a release resource method";
    `SDL2Renderer::clear` now clears renderer by calling `SDL_RenderClear`;
    
    `SDL2Renderer::present` now presents renderer by calling `SDL_RenderPresent`;
    
    `SDL2Renderer::drawingColor` with `color` argument now sets new drawing color,
      by calling `SDL_SetRenderDrawColor` with `_handle(aka .get())` and colors from argument

--- src/graphics/SDL2Renderer.cpp
+++ src/graphics/SDL2Renderer.cpp
@@ -4,18 +4,29 @@ namespace vulture{
     SDL2Renderer::SDL2Renderer(std::weak_ptr<SDL2Window> window):
         Renderer(window)
     {
-
+        SDL2Window::HandleProvider provider;
+        auto hwindow = provider.provide(*window.lock().get());
+        _handle = renderer_handle(
+            SDL_CreateRenderer(hwindow.lock().get(), -1, SDL_RENDERER_ACCELERATED),
+            SDL_DestroyRenderer
+        );
     }
 
     void
     SDL2Renderer::clear(){
+        SDL_RenderClear(_handle.get());
     }
 
     void
     SDL2Renderer::present(){
+        SDL_RenderPresent(_handle.get());
     }
 
     void
-    SDL2Renderer::drawingColor(const Color &){
+    SDL2Renderer::drawingColor(const Color &color){
+        SDL_SetRenderDrawColor(
+            _handle.get(),
+            color.r, color.g, color.b, color.a
+        );
     }
 }
