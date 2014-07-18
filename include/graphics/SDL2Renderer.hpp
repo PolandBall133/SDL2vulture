5c814f9e11d0e296f54151704254010ab165a104
spartanPAGE <spartanpageagain@gmail.com>
2014-07-18 14:23:15 +0200

    `SDL2Renderer` now publicly extends Renderer; All methods of SDL2Renderer are now tagged as public

--- include/graphics/SDL2Renderer.hpp
+++ include/graphics/SDL2Renderer.hpp
@@ -2,8 +2,11 @@
 
 #include "graphics/Renderer.hpp"
 
+#include <memory>
+
 namespace vulture{
-    class SDL2Renderer : Renderer{
+    class SDL2Renderer : public Renderer{
+    public:
         virtual void clear() override;
         virtual void present() override;
         virtual void drawingColor(const Color &) override;
