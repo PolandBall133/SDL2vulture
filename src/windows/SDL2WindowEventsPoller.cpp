99162f44a177fa9d593874c98caa18e24e32d701
spartanPAGE <spartanpageagain@gmail.com>
2014-07-20 14:00:42 +0200

    In `SDL2WindowEventsPoller::pollEvents`:   - added Polling loop

--- src/windows/SDL2WindowEventsPoller.cpp
+++ src/windows/SDL2WindowEventsPoller.cpp
@@ -1,7 +1,12 @@
 #include "windows/SDL2WindowEventsPoller.hpp"
+#include "SDL2/SDL.h"
 
 namespace vulture{
     void
     SDL2WindowEventsPoller::pollEvents(){
+        SDL_Event e;
+        while(SDL_PollEvent(&e)){
+
+        }
     }
 }
