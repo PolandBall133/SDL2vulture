591bc317a1c86430bb50714a4714c5903e1d8c3b
spartanPAGE <spartanpageagain@gmail.com>
2014-07-20 14:38:38 +0200

    in `SDL2WindowEventsPoller` method:   - create sdl_event   - while we can poll sdl events:     - create SDL2WindowEvent

--- src/windows/SDL2WindowEventsPoller.cpp
+++ src/windows/SDL2WindowEventsPoller.cpp
@@ -1,12 +1,14 @@
 #include "windows/SDL2WindowEventsPoller.hpp"
+#include "windows/SDL2WindowEvents.hpp"
 #include "SDL2/SDL.h"
 
 namespace vulture{
     void
     SDL2WindowEventsPoller::pollEvents(){
-        SDL_Event e;
-        while(SDL_PollEvent(&e)){
-
+        SDL_Event sdl_event;
+        while(SDL_PollEvent(&sdl_event)){
+            SDL2WindowEvent event(sdl_event);
+            /* TODO: calls window methods like {onMove}? calls {handleEvent}? */
         }
     }
 }
