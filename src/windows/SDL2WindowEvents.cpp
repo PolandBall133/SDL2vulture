ddaf5992d1dc4d5b1203b6b5f2ee82b703cc2746
spartanPAGE <spartanpageagain@gmail.com>
2014-07-20 14:37:41 +0200

    In `SDL2WIndowEvent` constructor:   - create static map with both SDL and vulture event types   - if event was found in map, set a type

--- src/windows/SDL2WindowEvents.cpp
+++ src/windows/SDL2WindowEvents.cpp
@@ -1,7 +1,23 @@
 #include "windows/SDL2WindowEvents.hpp"
 #include "SDL2/SDL.h"
 
+#include <map>
+
 namespace vulture{
+
     SDL2WindowEvent::SDL2WindowEvent(SDL_Event &e){
+        static const std::map<uint32_t, WindowEvent::Type> types_map = {
+            {SDL_WINDOWEVENT_SHOWN,     WindowEvent::Type::Shown},
+            {SDL_WINDOWEVENT_EXPOSED,   WindowEvent::Type::Exposed},
+            {SDL_WINDOWEVENT_MOVED,     WindowEvent::Type::Moved},
+            {SDL_WINDOWEVENT_RESIZED,   WindowEvent::Type::Resized},
+            {SDL_WINDOWEVENT_MINIMIZED, WindowEvent::Type::Minimized},
+            {SDL_WINDOWEVENT_MAXIMIZED, WindowEvent::Type::Maximized},
+            {SDL_WINDOWEVENT_RESTORED,  WindowEvent::Type::Restored},
+            {SDL_WINDOWEVENT_CLOSE,     WindowEvent::Type::Closed}
+        };
+        if(e.type != SDL_WINDOWEVENT &&
+           types_map.count(e.window.event))
+            _type = types_map.at(e.window.event);
     }
 }
