ecb888ebb8bb7a384441511fec39ba5267599ed1
spartanPAGE <spartanpageagain@gmail.com>
2014-07-22 10:07:37 +0200

    Fixed `_type` in `SDL2WindowEvent` constructor (now use double std::array instead of map. which sucks)

--- src/windows/SDL2WindowEvents.cpp
+++ src/windows/SDL2WindowEvents.cpp
@@ -1,23 +1,39 @@
 #include "windows/SDL2WindowEvents.hpp"
 #include "SDL2/SDL.h"
 
-#include <map>
+#include <array>
+using namespace std;
 
 namespace vulture{
 
     SDL2WindowEvent::SDL2WindowEvent(SDL_Event &e){
-        static const std::map<uint32_t, WindowEvent::Type> types_map = {
-            {SDL_WINDOWEVENT_SHOWN,     WindowEvent::Type::Shown},
-            {SDL_WINDOWEVENT_EXPOSED,   WindowEvent::Type::Exposed},
-            {SDL_WINDOWEVENT_MOVED,     WindowEvent::Type::Moved},
-            {SDL_WINDOWEVENT_RESIZED,   WindowEvent::Type::Resized},
-            {SDL_WINDOWEVENT_MINIMIZED, WindowEvent::Type::Minimized},
-            {SDL_WINDOWEVENT_MAXIMIZED, WindowEvent::Type::Maximized},
-            {SDL_WINDOWEVENT_RESTORED,  WindowEvent::Type::Restored},
-            {SDL_WINDOWEVENT_CLOSE,     WindowEvent::Type::Closed}
+        static const array<uint32_t, 8> sdl2_types = {
+            SDL_WINDOWEVENT_SHOWN,
+            SDL_WINDOWEVENT_EXPOSED,
+            SDL_WINDOWEVENT_MOVED,
+            SDL_WINDOWEVENT_RESIZED,
+            SDL_WINDOWEVENT_MINIMIZED,
+            SDL_WINDOWEVENT_MAXIMIZED,
+            SDL_WINDOWEVENT_RESTORED,
+            SDL_WINDOWEVENT_CLOSE
         };
-        if(e.type != SDL_WINDOWEVENT &&
-           types_map.count(e.window.event))
-            _type = types_map.at(e.window.event);
+        static const array<WindowEvent::Type, 8> vulture_types = {
+            WindowEvent::Type::Shown,
+            WindowEvent::Type::Exposed,
+            WindowEvent::Type::Moved,
+            WindowEvent::Type::Resized,
+            WindowEvent::Type::Minimized,
+            WindowEvent::Type::Maximized,
+            WindowEvent::Type::Restored,
+            WindowEvent::Type::Closed
+        };
+        if(e.type != SDL_WINDOWEVENT)
+            return;
+
+        for(size_t i = 0; i < sdl2_types.size(); ++i)
+            if(e.window.event == sdl2_types[i]){
+                _type = vulture_types[i];
+                return;
+            }
     }
 }
