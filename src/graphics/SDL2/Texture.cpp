d82ca5684e9ada4d2552f01d592af085f7b71c94
spartanPAGE <spartanpageagain@gmail.com>
2014-07-25 00:56:17 +0200

    Added empty definition of `SDL2::Texture::load` method

--- src/graphics/SDL2/Texture.cpp
+++ src/graphics/SDL2/Texture.cpp
@@ -1,8 +1,11 @@
 #include "graphics/SDL2/Renderer.hpp"
-
+using namespace std;
 namespace vulture{
     namespace graphics{
         namespace SDL2{
+            void
+            Texture::load(SDL2::Renderer &renderer, const string &name){
+            }
         }
     }
 }
