a45576b2cb788c0e3c37f24d2c983b8524e5350c
spartanPAGE <spartanpageagain@gmail.com>
2014-07-25 00:49:43 +0200

    Added `SDL2/Texture` with `SDL2::Texture < vulture::Texture` empty class

---
+++ include/graphics/SDL2/Texture.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "graphics/Renderer.hpp"
+
+#include "SDL2/SDL.h"
+
+#include <memory>
+
+namespace vulture{
+    namespace graphics{
+        namespace SDL2{
+            class Texture : public vulture::Texture{
+            };
+        }
+    }
+}
