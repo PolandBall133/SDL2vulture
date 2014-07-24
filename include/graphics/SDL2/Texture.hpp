ea02806f80d472c7ebd61a0aab4a4c1da9a4c350
spartanPAGE <spartanpageagain@gmail.com>
2014-07-25 00:51:36 +0200

    `SDL2::Texture` now overrides `load(renderer(SDL2), name)` method

--- include/graphics/SDL2/Texture.hpp
+++ include/graphics/SDL2/Texture.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
-#include "graphics/Renderer.hpp"
+#include "graphics/SDL2/Renderer.hpp"
 
 #include "SDL2/SDL.h"
 
 #include <memory>
+#include <string>
 
 namespace vulture{
     namespace graphics{
         namespace SDL2{
             class Texture : public vulture::Texture{
+            public:
+                virtual void load(SDL2::Renderer &, const std::string &) override;
             };
         }
     }
