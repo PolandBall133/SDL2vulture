ead745a3146b7e92ec79546f84f874a815eca508
spartanPAGE <spartanpageagain@gmail.com>
2014-07-25 01:03:15 +0200

    Added definition of `SDL2::Texture::color(val)`

--- src/graphics/SDL2/Texture.cpp
+++ src/graphics/SDL2/Texture.cpp
@@ -1,10 +1,18 @@
 #include "graphics/SDL2/Renderer.hpp"
+#include "graphics/SDL2/Texture.hpp"
 using namespace std;
 namespace vulture{
     namespace graphics{
         namespace SDL2{
             void
-            Texture::load(SDL2::Renderer &renderer, const string &name){
+            Texture::load(graphics::Renderer &renderer, const string &name){
+            }
+
+            void
+            Texture::color(const Color &val){
+                graphics::Texture::color(val);
+                SDL_SetTextureColorMod(_handle.get(), val.r, val.g, val.b);
+                SDL_SetTextureAlphaMod(_handle.get(), val.a);
             }
         }
     }
