b27d77799bb3f7826aff7a495945db400270baaa
spartanPAGE <spartanpageagain@gmail.com>
2014-07-26 01:28:04 +0200

    Fixed `SDL2::Texture` parent type; Added private `SDL::Texture::base` typedef

--- include/graphics/SDL2/Texture.hpp
+++ include/graphics/SDL2/Texture.hpp
@@ -11,14 +11,16 @@
 namespace vulture{
     namespace graphics{
         namespace SDL2{
-            class Texture : public graphics::Texture{
+            class Texture : public graphics::Texture<SDL2::Renderer>{
             public:
-                virtual void load(graphics::Renderer &, const std::string &) override;
+                virtual void load(SDL2::Renderer &, const std::string &) override;
 
                 virtual void color(const Color &) override;
             protected:
                 typedef std::shared_ptr<SDL_Texture> texture_handle;
                 texture_handle _handle;
+            private:
+                typedef graphics::Texture<SDL2::Renderer> base;
             };
         }
     }
