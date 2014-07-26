0f928b9552506376b55862d0c2e42404e325b78c
spartanPAGE <spartanpageagain@gmail.com>
2014-07-26 02:39:47 +0200

    some definitions

--- src/graphics/SDL2/Texture.cpp
+++ src/graphics/SDL2/Texture.cpp
@@ -2,11 +2,35 @@
 #include "graphics/SDL2/Texture.hpp"
 
 using namespace std;
+
 namespace vulture{
     namespace graphics{
         namespace SDL2{
+            Texture::Texture(weak_ptr<SDL2::Renderer> renderer):
+                base(renderer){}
+
             void
-            Texture::load(SDL2::Renderer &renderer, const string &name){
+            Texture::load(const string &name){
+                SDL2::Renderer::HandleProvider provider;
+                auto renderer = _renderer_handle.lock().get();
+                auto hrenderer = provider.provide(*renderer);
+                SDL_Surface *surface = SDL_LoadBMP(name.c_str());
+                _handle = texture_handle{
+                    SDL_CreateTextureFromSurface(
+                        hrenderer.lock().get(),
+                        surface
+                    ),
+                    SDL_DestroyTexture
+                };
+                SDL_FreeSurface(surface);
+            }
+
+            const set<string> &
+            Texture::supportedFormats() const{
+                static set<string> supported_formats = {
+                    "bmp"
+                };
+                return supported_formats;
             }
 
             void
