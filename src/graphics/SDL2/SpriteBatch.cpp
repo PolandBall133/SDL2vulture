1462cee769a0a60255f121ecb8d9fe4f65f30b2d
spartanPAGE <spartanpageagain@gmail.com>
2014-07-28 00:22:29 +0200

    Added defintion of `SDL2SpriteBatch(renderer_handle)` constructor

--- src/graphics/SDL2/SpriteBatch.cpp
+++ src/graphics/SDL2/SpriteBatch.cpp
@@ -1,8 +1,11 @@
 #include "graphics/SDL2/SpriteBatch.hpp"
 
+using namespace std;
 namespace vulture{
     namespace graphics{
         namespace SDL2{
+            SpriteBatch::SpriteBatch(renreder_handle hrenderer):
+                _renderer_handle(hrenderer){}
         }
     }
 }
