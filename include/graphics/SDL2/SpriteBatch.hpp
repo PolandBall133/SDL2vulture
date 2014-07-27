f31c4c4a15c190f822cac96c7e03a561065f48d2
spartanPAGE <spartanpageagain@gmail.com>
2014-07-28 00:07:56 +0200

    Added `graphics/SDL2/SpriteBatch` header

---
+++ include/graphics/SDL2/SpriteBatch.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "graphics/SpriteBatch.hpp"
+#include "graphics/SDL2/Renderer.hpp"
+
+#include "SDL2/SDL.h"
+
+#include <memory>
+
+namespace vulture{
+    namespace SDL2{
+    }
+}
