13468b7f91c8bb88513162b4220d79344f1480c9
spartanPAGE <spartanpageagain@gmail.com>
2014-07-28 00:22:09 +0200

    In `SDL2::SpriteBatch`:   refractored:     - constructor   added:     - protected `renderer_handle` typedef     - protected `_renderer_handle` field

--- include/graphics/SDL2/SpriteBatch.hpp
+++ include/graphics/SDL2/SpriteBatch.hpp
@@ -14,8 +14,10 @@ namespace vulture{
                 typedef graphics::SpriteBatch base;
             protected:
                 struct Drawer : public base::Drawer{};
+                typedef std::weak_ptr<SDL2::Renderer> renderer_handle;
+                renderer_handle _renderer_handle;
             public:
-                SpriteBatch(std::weak_ptr<SDL2::Renderer>);
+                SpriteBatch(renderer_handle);
                 virtual void begin() override;
                 virtual void end() override;
             };
