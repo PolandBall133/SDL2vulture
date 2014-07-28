55e2f3712e883b9a7997bafbe386767c88cc8bc1
spartanPAGE <spartanpageagain@gmail.com>
2014-07-28 13:31:14 +0200

    Changed default value of `Point` in `SpriteBatch::Drawer::texture` overrided `SpriteBatch::draw`

--- include/graphics/SDL2/SpriteBatch.hpp
+++ include/graphics/SDL2/SpriteBatch.hpp
@@ -12,27 +12,34 @@ namespace vulture{
     namespace graphics{
         namespace SDL2{
             class SpriteBatch : public graphics::SpriteBatch<SDL2::Texture>{
+            public:
                 typedef graphics::SpriteBatch<SDL2::Texture> base;
             protected:
                 typedef std::weak_ptr<SDL2::Renderer> renderer_handle;
+
                 renderer_handle _renderer_handle;
 
                 Color _bgcolor;
 
-                struct Drawer : public base::Drawer{
+                class Drawer : public base::Drawer{
+                protected:
+                    typedef std::weak_ptr<SDL_Renderer> renderer_handle;
+                    renderer_handle _renderer_handle;
+                public:
+                    Drawer(renderer_handle);
                     virtual void texture(
                         SDL2::Texture &,
                         const Vector2 &,
                         const Rectangle &,
                         double = 0.0,
-                        const Point = Point::zero()
+                        const Point = -Point::one()
                     ) override;
 
                     virtual void texture(
                         SDL2::Texture &,
                         const Vector2 &,
                         double = 0.0,
-                        const Point = Point::zero()
+                        const Point = -Point::one()
                     ) override;
 
                     virtual void quad(
@@ -46,13 +53,16 @@ namespace vulture{
                         const Color &
                     ) override;
                 };
+                std::shared_ptr<Drawer> _drawerptr;
             public:
                 SpriteBatch(renderer_handle);
+
                 virtual void begin() override;
                 virtual void end() override;
 
                 virtual const Color backgroundColor() const override;
                 virtual void backgroundColor(const Color &) override;
+                virtual base::Drawer &draw() override;
             };
         }
     }
