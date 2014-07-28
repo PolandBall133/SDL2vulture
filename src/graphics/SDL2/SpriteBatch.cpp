00ee07991ba4bab17a1ef88ee55f134471af7570
spartanPAGE <spartanpageagain@gmail.com>
2014-07-28 13:31:23 +0200

    Added some drawing logic.

--- src/graphics/SDL2/SpriteBatch.cpp
+++ src/graphics/SDL2/SpriteBatch.cpp
@@ -4,14 +4,36 @@ using namespace std;
 namespace vulture{
     namespace graphics{
         namespace SDL2{
+            SpriteBatch::Drawer::Drawer(renderer_handle h):
+                _renderer_handle(h){}
+
             void
             SpriteBatch::Drawer::texture(
                 SDL2::Texture &texture,
                 const Vector2 &position,
                 const Rectangle &rect,
                 double angle,
-                const Point
+                const Point point
             ){
+                bool pointProvided = point != -Point::one();
+                SDL_RendererFlip rf;
+                SDL_Rect clip = { rect.x, rect.y, rect.width, rect.height },
+                         quad = { (int)position.x, (int)position.y, clip.w, clip.h };
+
+                SDL_Point p;
+                if(pointProvided) p = SDL_Point{ point.x, point.y };
+
+                SDL2::Texture::HandleProvider hp;
+
+                SDL_RenderCopyEx(
+                    _renderer_handle.lock().get(),
+                    hp.provide(texture).lock().get(),
+                    &clip,
+                    &quad,
+                    angle,
+                    pointProvided? &p : NULL,
+                    rf
+                );
             }
 
             void
@@ -19,7 +41,7 @@ namespace vulture{
                 SDL2::Texture &texture,
                 const Vector2 &position,
                 double angle,
-                const Point
+                const Point point
             ){
             }
 
@@ -28,6 +50,8 @@ namespace vulture{
                 const Rectangle &rect,
                 const Color &color
             ){
+                //renderer->drawingColor(backgroundColor());
+                SDL_Rect r = { rect.x, rect. y, rect.width, rect.height };
             }
 
             void
@@ -40,7 +64,11 @@ namespace vulture{
 
             SpriteBatch::SpriteBatch(renderer_handle renderer):
                 _renderer_handle(renderer),
-                _bgcolor(Color::White()){}
+                _bgcolor(Color::White())
+            {
+                SDL2::Renderer::HandleProvider p;
+                _drawerptr = make_shared<Drawer>(p.provide(*renderer.lock().get()));
+            }
 
             void
             SpriteBatch::begin(){
@@ -63,6 +91,11 @@ namespace vulture{
             void
             SpriteBatch::backgroundColor(const Color &color){
                 _bgcolor = color;
+            }
+
+            SpriteBatch::base::Drawer &
+            SpriteBatch::draw(){
+                return *_drawerptr.get();
             }
         }
     }
