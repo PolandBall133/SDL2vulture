5455518f86611dcdac83d5ec41fef3f6f51ecb10
spartanPAGE <spartanpageagain@gmail.com>
2014-07-28 11:00:25 +0200

    Overrided `graphics::SpriteBatch::Drawer` in `SLD2::SpriteBatch::Drawer`

--- include/graphics/SDL2/SpriteBatch.hpp
+++ include/graphics/SDL2/SpriteBatch.hpp
@@ -2,6 +2,7 @@
 
 #include "graphics/SpriteBatch.hpp"
 #include "graphics/SDL2/Renderer.hpp"
+#include "graphics/SDL2/Texture.hpp"
 
 #include "SDL2/SDL.h"
 
@@ -10,15 +11,41 @@
 namespace vulture{
     namespace graphics{
         namespace SDL2{
-            class SpriteBatch : public graphics::SpriteBatch{
-                typedef graphics::SpriteBatch base;
+            class SpriteBatch : public graphics::SpriteBatch<SDL2::Texture>{
+                typedef graphics::SpriteBatch<SDL2::Texture> base;
             protected:
-                struct Drawer : public base::Drawer{};
-
                 typedef std::weak_ptr<SDL2::Renderer> renderer_handle;
                 renderer_handle _renderer_handle;
 
                 Color _bgcolor;
+
+                struct Drawer : public base::Drawer{
+                    virtual void texture(
+                        SDL2::Texture &,
+                        const Vector2 &,
+                        const Rectangle &,
+                        double = 0.0,
+                        const Point = Point::zero()
+                    ) override;
+
+                    virtual void texture(
+                        SDL2::Texture &,
+                        const Vector2 &,
+                        double = 0.0,
+                        const Point = Point::zero()
+                    ) override;
+
+                    virtual void quad(
+                        const Rectangle &,
+                        const Color &
+                    ) override;
+
+                    virtual void line(
+                        const Point &,
+                        const Point &,
+                        const Color &
+                    ) override;
+                };
             public:
                 SpriteBatch(renderer_handle);
                 virtual void begin() override;
